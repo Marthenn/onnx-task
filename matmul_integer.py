cff210a229ba73a4b3d03c5dfd020b78e322ebbc
Bintang Dwi Marthen <89301265+Marthenn@users.noreply.github.com>
2024-11-19 23:27:56 +0700

    refactor (matmul): made shape defined for matmul_integer

--- matmul_integer.py
+++ matmul_integer.py
@@ -2,9 +2,9 @@ import onnx
 from onnx import helper, TensorProto
 
 def matmul_integer():
-    input_A = helper.make_tensor_value_info('input_A', TensorProto.INT8, [None, None])
-    input_B = helper.make_tensor_value_info('input_B', TensorProto.INT8, [None, None])
-    output_Y = helper.make_tensor_value_info('output_Y', TensorProto.INT32, [None, None])
+    input_A = helper.make_tensor_value_info('input_A', TensorProto.INT8, [2, 2])
+    input_B = helper.make_tensor_value_info('input_B', TensorProto.INT8, [2, 2])
+    output_Y = helper.make_tensor_value_info('output_Y', TensorProto.INT32, [2, 2])
 
     matmul_node = helper.make_node(
         'MatMulInteger',
