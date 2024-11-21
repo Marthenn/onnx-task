18beafb150445972d859980f922986f20c530f63
Marthenn <89301265+Marthenn@users.noreply.github.com>
2024-11-22 00:26:25 +0700

    feat (task2): task2 code

---
+++ inject_utils/layers.py
@@ -0,0 +1,223 @@
+from onnx import helper, ModelProto, TensorProto, OperatorSetIdProto, shape_inference
+from qonnx.core.onnx_exec import execute_onnx
+#from inject_utils.utils import *
+import numpy as np
+import struct
+
+def fp32tobin(value):
+    return ''.join(bin(c).replace('0b', '').rjust(8, '0') for c in struct.pack('!f', value))
+
+def bin2fp32(bin_str):
+    assert len(bin_str) == 32
+    data = struct.unpack('!f',struct.pack('!I', int(bin_str, 2)))[0]
+    if np.isnan(data):
+        return 0
+    else:
+        return data
+
+def delta_init():
+    one_bin = ''
+    for _ in range(32):
+        one_bin += str(np.random.randint(0,2))
+    return bin2fp32(one_bin)
+
+def float32_bit_flip(faulty_tensor, target_indices):
+    golden_value = faulty_tensor[tuple(target_indices)]
+    golden_string = fp32tobin(golden_value)
+    flip_bit = np.random.randint(32)
+    if golden_string[31-flip_bit] == '1':
+        inject_string = golden_string[:31-flip_bit] + '0' + golden_string[31-flip_bit+1:]
+    else:
+        inject_string = golden_string[:31-flip_bit] + '1' + golden_string[31-flip_bit+1:]
+    faulty_value = bin2fp32(inject_string)
+    return faulty_value, flip_bit
+
+def float16_bit_flip(faulty_tensor, target_indices, bit_position=None):
+    golden_value = faulty_tensor[tuple(target_indices)]
+    golden_string = fp16tobin(golden_value)
+    flip_bit = np.random.randint(16)
+    if bit_position is not None:
+        flip_bit = bit_position
+    if golden_string[15-flip_bit] == '1':
+        inject_string = golden_string[:15-flip_bit] + '0' + golden_string[15-flip_bit+1:]
+    else:
+        inject_string = golden_string[:15-flip_bit] + '1' + golden_string[15-flip_bit+1:]
+    faulty_value = bin2fp16(inject_string)
+    return faulty_value, flip_bit
+
+def flip_int4_bit(value, bit_position):
+    mask = 1 << bit_position
+    flipped_value = value ^ mask
+    if flipped_value > 7:
+        flipped_value -= 16
+    if flipped_value < -8:
+        flipped_value += 16
+    print("VALUE")
+    print(value)
+    print("FLIPPED")
+    print(flipped_value)
+    return flipped_value
+
+def flip_int8_bit(value, bit_position):
+    mask = 1 << bit_position
+    flipped_value = value ^ mask
+    if flipped_value > 127:
+        flipped_value -= 256
+    if flipped_value < -128:
+        flipped_value += 256
+    return flipped_value
+
+def flip_uint8_bit(value, bit_position):
+    mask = 1 << bit_position
+    flipped_value = value ^ mask
+    if flipped_value > 255:
+        flipped_value -= 256
+    if flipped_value < 0:
+        flipped_value += 256
+    return flipped_value
+
+def int_bit_flip(input_dict, target_tensor, target_bit_position, bit_precision=4):
+    faulty_tensor = input_dict[target_tensor]
+    faulty_tensor = np.int8(faulty_tensor)
+    random_indices = [np.random.randint(0, dim) for dim in faulty_tensor.shape]
+    """
+    print("ORIGINAL VALUE:")
+    print(faulty_tensor[tuple(random_indices)])
+    """
+    faulty_value = flip_int8_bit(faulty_tensor[tuple(random_indices)], target_bit_position)
+    assert faulty_value >= -128 and faulty_value <= 127
+    """
+    faulty_value = flip_int4_bit(faulty_tensor[tuple(random_indices)], target_bit_position)
+    assert faulty_value >= -8 and faulty_value <= 7
+    """
+    return faulty_value, random_indices
+
+def uint_bit_flip(input_dict, target_tensor, target_bit_position, bit_precision=4):
+    faulty_tensor = input_dict[target_tensor]
+    faulty_tensor = np.uint8(faulty_tensor)
+    random_indices = [np.random.randint(0, dim) for dim in faulty_tensor.shape]
+    """
+    print("ORIGINAL VALUE:")
+    print(faulty_tensor[tuple(random_indices)])
+    """
+    faulty_value = flip_uint8_bit(faulty_tensor[tuple(random_indices)], target_bit_position)
+    assert faulty_value >= 0 and faulty_value <= 255
+    """
+    faulty_value = flip_int4_bit(faulty_tensor[tuple(random_indices)], target_bit_position)
+    assert faulty_value >= -8 and faulty_value <= 7
+    """
+    return faulty_value, random_indices
+
+def perturb_quantizer(graph, node, module, model, input_dict, weight_dict, faulty_tensor_name, faulty_bit_position):
+    if "ConvInteger" in node.op_type:
+        faulty_value, target_indices = uint_bit_flip(weight_dict, faulty_tensor_name, faulty_bit_position, 4)
+    else:
+        faulty_value, target_indices = int_bit_flip(weight_dict, faulty_tensor_name, faulty_bit_position, 4)
+    """
+    print("FAULTY VALUE:")
+    print(faulty_value)
+    
+    print("INPUT")
+    print(input_dict[list(input_dict.keys())[0]].shape)
+    print(input_dict[list(input_dict.keys())[1]].shape)
+    """
+    golden_value = weight_dict[faulty_tensor_name][tuple(target_indices)]
+    is_signed = ""
+    if isinstance(golden_value, np.uint8):
+        is_signed = "Unsigned"
+    else:
+        is_signed = "Signed"
+    original_tensor_value = input_dict[faulty_tensor_name]
+
+    input_perturb = np.zeros(weight_dict[faulty_tensor_name].shape, dtype=weight_dict[faulty_tensor_name].dtype)
+    input_perturb[tuple(target_indices)] = faulty_value
+    input_dict[faulty_tensor_name] = input_perturb
+
+    """
+    print("INPUT INNER:")
+    print(input_dict[list(input_dict.keys())[0]].shape)
+    print(input_dict[list(input_dict.keys())[1]].shape)
+    print("FOCUS GRAPH:")
+
+    print(graph)
+    """
+    if module == "Decoder":
+        replacement_dictionary = {
+            "onnx::ReduceMean_0_dynamic_axes_1": weight_dict["global_in"].shape[1],
+            "onnx::Unsqueeze_3_dynamic_axes_1": weight_dict["global_in_3"].shape[1],
+            "onnx::Unsqueeze_3_dynamic_axes_2": weight_dict["global_in_3"].shape[2],
+        }
+        for output in model.graph.output:
+            for dimension in range(len(output.type.tensor_type.shape.dim)):
+                for key in replacement_dictionary.keys():
+                    if key in str(output.type.tensor_type.shape.dim[dimension]):
+                        output.type.tensor_type.shape.dim[dimension].Clear()
+                        output.type.tensor_type.shape.dim[dimension].dim_value = replacement_dictionary[key]
+                    if "unk__" in str(output.type.tensor_type.shape.dim[dimension]):
+                        output.type.tensor_type.shape.dim[dimension].Clear()
+                        output.type.tensor_type.shape.dim[dimension].dim_value = weight_dict[output.name].shape[dimension]
+
+    output_tensors = execute_onnx(model, input_dict)
+    weight_dict["delta_4d"] = output_tensors[(list(output_tensors.keys())[0])]
+    random_indices = [np.random.randint(0, dim) for dim in weight_dict["delta_4d"].shape]
+    faulty_index_value = weight_dict["delta_4d"][tuple(random_indices)]
+
+    dequantized_result_tensor_name = list(output_tensors.keys())[0]
+    perturb = weight_dict["delta_4d"][tuple(random_indices)] - weight_dict[dequantized_result_tensor_name][tuple(random_indices)]
+    weight_dict["delta_4d"][tuple(random_indices)] = perturb
+
+    return weight_dict#, dequantized_result_tensor_name, target_indices, golden_value, faulty_value, is_signed
+
+
+def perturb_fp16(model, input_dict, weight_dict, faulty_tensor_name, faulty_bit_position):
+    target_indices = [np.random.randint(0, dim) for dim in weight_dict[faulty_tensor_name].shape]
+    faulty_value, _ = float16_bit_flip(input_dict[faulty_tensor_name], target_indices, faulty_bit_position)
+    golden_value = weight_dict[faulty_tensor_name][tuple(target_indices)]
+    is_signed = "float16"
+    original_tensor_value = input_dict[faulty_tensor_name]
+
+    input_perturb = np.zeros(weight_dict[faulty_tensor_name].shape, dtype=weight_dict[faulty_tensor_name].dtype)
+    input_perturb[tuple(target_indices)] = faulty_value
+    input_dict[faulty_tensor_name] = input_perturb
+
+    output_tensors = execute_onnx(model, input_dict)
+    weight_dict["delta_4d"] = output_tensors[(list(output_tensors.keys())[0])]
+    faulty_index_value = weight_dict["delta_4d"][tuple(target_indices)]
+
+    dequantized_result_tensor_name = list(output_tensors.keys())[0]
+    perturb = weight_dict["delta_4d"][tuple(target_indices)] - weight_dict[dequantized_result_tensor_name][tuple(target_indices)]
+    weight_dict["delta_4d"][tuple(target_indices)] = perturb
+
+    return weight_dict, dequantized_result_tensor_name, target_indices, golden_value, faulty_value, is_signed
+
+def perturb_conv(model, input_dict, weight_dict, input_tensor_name, bias_output_name):
+    input_dict[input_tensor_name] = weight_dict["delta_4d"]
+    no_bias = np.zeros(weight_dict[bias_output_name].shape, dtype=weight_dict[bias_output_name].dtype)
+    input_dict[bias_output_name] = no_bias
+    delta_perturb = execute_onnx(model, input_dict)
+    delta_perturb = delta_perturb[list(delta_perturb.keys())[0]]
+    return delta_perturb
+
+def perturb_matmul(model, input_dict, weight_dict, input_tensor_name, transposed_axes=None):
+    targetted_axes = None
+    if transposed_axes:
+        if (transposed_axes.input[0] in input_tensor_name):
+            targetted_axes = list(transposed_axes.attribute[0].ints)
+            input_tensor_name = transposed_axes.output[0]
+            weight_dict["delta_4d"] = np.transpose(weight_dict["delta_4d"], tuple(targetted_axes))
+    input_dict[input_tensor_name] = weight_dict["delta_4d"]
+    delta_perturb = execute_onnx(model, input_dict)
+    delta_perturb = delta_perturb[list(delta_perturb.keys())[0]]
+
+    return delta_perturb
+
+"""
+def get_perturbation(golden_value, bit_position=None, fault_model=None):
+    golden_value = np.int8(golden_value)
+    if fault_model == "RANDOM":
+        for _ in range(32):
+            inj_bin += str(np.random.randint(0,2))
+        perturb = bin2fp16(inj_bin) - golden_value
+    else:
+        flipped_value = flip_int8_bit(golden_value, bit_position)
+"""
---
+++ task2.py
@@ -0,0 +1,231 @@
+from onnx import helper, ModelProto, TensorProto, OperatorSetIdProto, shape_inference
+from qonnx.core.modelwrapper import ModelWrapper
+from qonnx.core.onnx_exec import execute_onnx
+import onnx.numpy_helper as numpy_helper
+import numpy as np
+import torch
+import onnx
+
+from onnx.shape_inference import infer_shapes
+from inject_utils.layers import perturb_quantizer
+from inject_utils.layers import float32_bit_flip
+from inject_utils.layers import delta_init
+
+import time
+import copy
+
+def execute_node(node, main_graph, final_output_node, weight_dict, module, inject_parameters=None):
+    node_inputs = []
+    node_outputs = []
+
+    added_quant_inputs, added_quant_outputs, list_operation_time = expand_node_inputs_outputs(main_graph, node, weight_dict, module)
+    node_inputs += added_quant_inputs
+    node_outputs += added_quant_outputs
+
+    desired_node_outputs = [x for x in node_outputs if x.name == final_output_node]
+    intermediate_node_outputs = [x for x in node_outputs if x.name != final_output_node]
+    
+    for index, node_input in enumerate(node.input):
+        if (len(node_input)) == 0:
+            node.input[index] = node_inputs[-1].name
+
+    graph = helper.make_graph(
+            nodes = [node],
+            name = "single_node_exec",
+            inputs = node_inputs,
+            outputs = desired_node_outputs
+    )
+
+    model = ModelProto()
+    model = infer_shapes(model)
+    model.graph.CopyFrom(graph)
+    model.opset_import.append(OperatorSetIdProto(version=13))
+    model = ModelWrapper(model)
+
+    input_dict = {}
+    for node_iter in node_inputs:
+        if node_iter.name == [node_intermediate.name for node_intermediate in intermediate_node_outputs]:
+            continue
+        if node_iter.name in [node_intermediate.name for node_intermediate in node_outputs]:
+            continue
+        input_dict[node_iter.name] = weight_dict[node_iter.name]
+
+    output_tensors = execute_onnx(model, input_dict)
+    tensor_output_name = list(output_tensors.keys())[0]
+    original_tensor_output = output_tensors[tensor_output_name]
+    weight_dict[tensor_output_name] = output_tensors[tensor_output_name]
+
+    if inject_parameters and ("RANDOM" in inject_parameters["inject_type"]) and (node.op_type == inject_parameters["faulty_operation_name"]):
+        print("FOUND HERE RANDOM:")
+        print(node.name)
+        faulty_value = None
+        target_indices = [np.random.randint(0, dim) for dim in weight_dict[tensor_output_name].shape]
+        golden_value = weight_dict[tensor_output_name][tuple(target_indices)]
+        print(weight_dict[tensor_output_name][tuple(target_indices)])
+        if "BITFLIP" in inject_parameters["inject_type"]:
+            faulty_value, flip_bit = float32_bit_flip(weight_dict[tensor_output_name], target_indices)
+        else:
+            faulty_value = delta_init()
+        weight_dict[tensor_output_name][tuple(target_indices)] = faulty_value
+        print("FAULTY:")
+        print(faulty_value)
+
+    if inject_parameters and (inject_parameters["inject_type"] in ["INPUT", "WEIGHT", "INPUT16", "WEIGHT16"]):
+        # First layer in faulty_trace, obtains perturbations
+        if inject_parameters["faulty_tensor_name"] in node.input:
+            weight_dict = perturb_quantizer(graph, node, None, model, input_dict, weight_dict, inject_parameters["faulty_tensor_name"], inject_parameters["faulty_bit_position"])
+
+        # Final layer in faulty_trace, should be the target layer and applies the fault models
+        faulty_operation = inject_parameters["faulty_operation_name"]
+        if faulty_operation == inject_parameters["faulty_operation_name"]:
+            print("FINAL LAYER")
+            print(faulty_operation)
+            if "INPUT16" == inject_parameters["inject_type"]:
+                delta_16 = np.zeros(weight_dict["delta_4d"].shape, dtype=np.float32)
+                random_shape = list(weight_dict["delta_4d"].shape)
+                row_index = random_shape[3]//16
+                if row_index == 0:
+                    row_index = 0
+                else:
+                    row_index = np.random.randint(0, row_index)
+                row_index = row_index*16
+                indices = []
+                if len(np.nonzero(weight_dict["delta_4d"])[0]) > 0:
+                    for shape_index_array in np.nonzero(weight_dict["delta_4d"]):
+                        indices.append(list(shape_index_array)[0])
+                    indices[3] = row_index
+
+                    for i in range(16):
+                        if i >= random_shape[3]:
+                            break
+                        delta_16[tuple(indices)] = weight_dict["delta_4d"][(tuple(indices))]
+                        indices[3] = indices[3] + 1
+                    weight_dict["delta_4d"] = delta_16
+
+            elif "WEIGHT16" == inject_parameters["inject_type"]:
+                delta_16 = np.zeros(weight_dict["delta_4d"].shape, dtype=np.float32)
+                random_shape = list(weight_dict["delta_4d"].shape)
+                column_index = random_shape[2]//16
+                if column_index == 0:
+                    column_index = 0
+                else:
+                    column_index = np.random.randint(0, column_index)
+                column_index = column_index*16
+                indices = []
+                if len(np.nonzero(weight_dict["delta_4d"])[0]) > 0:
+                    for shape_index_array in np.nonzero(weight_dict["delta_4d"]):
+                        indices.append(list(shape_index_array)[0])
+                    indices[2] = column_index
+
+                    for i in range(np.random.randint(1,16)):
+                        if i >= random_shape[2]:
+                            break
+                        delta_16[tuple(indices)] = weight_dict["delta_4d"][(tuple(indices))]
+                        indices[2] = indices[2] + 1
+                    weight_dict["delta_4d"] = delta_16
+            else:
+                print("INPUTS/WEIGHTS")
+            print("FAULT INJECTED!")
+            print(np.nonzero(weight_dict["delta_4d"]))
+            temp_variable = (np.add(weight_dict[tensor_output_name], weight_dict["delta_4d"]))
+            weight_dict[tensor_output_name] = temp_variable
+            output_tensors[tensor_output_name] = temp_variable
+
+    if output_tensors is None:
+        print("HERE")
+        print(input_dict)
+        print(input_dict.keys())
+        print(node)
+        print(node.name)
+    return output_tensors, weight_dict, list_operation_time
+
+def inference(main_graph, weight_dict, module, inject_parameters=None):
+    def execute_single_node(node, weight_dict, main_graph, module):
+        final_output_node = node.output[0]
+        output_tensors, weight_dict, list_operation_time = execute_node(node, main_graph, final_output_node, weight_dict, module, inject_parameters)
+        return output_tensors, weight_dict, list_operation_time
+    output_tensors = None
+    for node in main_graph.node:
+        start_time = time.time()
+        output_tensors, weight_dict, list_operation_time = execute_single_node(node, weight_dict, main_graph, module)
+    return output_tensors, weight_dict
+
+def expand_node_inputs_outputs(graph, node, weight_dict, module):
+    added_inputs = []
+    added_outputs = []
+
+    added_inputs += list(filter(lambda x: x.name in node.input, graph.input))
+    added_inputs += list(filter(lambda x: x.name in node.input, graph.output))
+    added_inputs += list(filter(lambda x: x.name in node.input, graph.value_info))
+    added_outputs += list(filter(lambda x: x.name in node.output, graph.output))
+    added_outputs += list(filter(lambda x: x.name in node.output, graph.value_info))
+
+    start_time = time.time()
+    return added_inputs, added_outputs, time.time() - start_time
+
+def get_weight_dict(module_path):
+    module = ModelWrapper(module_path)
+    module_graph = module.graph
+    module_weights = module.graph.initializer
+    module_weight_dict = {}
+    for weight in module_weights:
+        module_weight_dict[weight.name] = numpy_helper.to_array(weight)
+    return module_graph, module_weight_dict
+
+def prepare_inference(module_path, module_input_values):
+    module = ModelWrapper(module_path)
+    output = [node.name for node in module.graph.output]
+
+    input_all = [node.name for node in module.graph.input]
+    input_initializers = [node.name for node in module.graph.initializer]
+    module_input_names = list(set(input_all) - set(input_initializers))
+
+    module_graph, module_weight_dict = get_weight_dict(module_path)
+
+    for input_name in module_input_names:
+        module_weight_dict[input_name] = module_input_values[input_name]
+
+    return module_weight_dict, module_graph
+
+def run_module(module, input_values, module_filepath, inject_parameters=None):
+    module_weight_dict, module_graph = prepare_inference(module_filepath, input_values)
+    for input_name in list(input_values.keys()):
+        module_weight_dict[input_name] = input_values[input_name]
+    return inference(module_graph, module_weight_dict, module, inject_parameters)
+
+if __name__ == "__main__":
+    # Matmul Integer Injection
+    # module_filepath = "matmul_integer.onnx"
+    # mat_A = np.random.randint(-128, 127, (1, 1, 2, 2)).astype(np.int8)
+    # mat_B = np.random.randint(-128, 127, (1, 1, 2, 2)).astype(np.int8)
+    # input_values = {
+    #     "input_A": mat_A,
+    #     "input_B": mat_B
+    # }
+    # inject_parameters = {}
+    # inject_parameters["inject_type"] = "WEIGHT"
+    # inject_parameters["faulty_tensor_name"] = "input_A"
+    # inject_parameters["faulty_bit_position"] = 1
+    # inject_parameters["faulty_output_tensor"] = "output_Y"
+    # inject_parameters["faulty_operation_name"] = "MatMulInteger"
+    # print(run_module(None, input_values, module_filepath, inject_parameters))
+
+    # Conv Integer Injection
+    module_filepath = "conv_integer.onnx"
+    mat_X = np.random.randint(0, 225, (1, 1, 3, 3)).astype(np.uint8)
+    mat_W = np.random.randint(0, 225, (1, 1, 2, 2)).astype(np.uint8)
+    zp_X = np.random.randint(0, 225, (1)).astype(np.uint8)
+    zp_W = np.random.randint(0, 225, (1)).astype(np.uint8)
+    input_values = {
+        "input_X": mat_X,
+        "input_W": mat_W,
+        "input_X_zero_point": zp_X,
+        "input_W_zero_point": zp_W
+    }
+    inject_parameters = {}
+    inject_parameters["inject_type"] = "WEIGHT"
+    inject_parameters["faulty_tensor_name"] = "input_X"
+    inject_parameters["faulty_bit_position"] = 0
+    inject_parameters["faulty_output_tensor"] = "output_Y"
+    inject_parameters["faulty_operation_name"] = "ConvInteger"
+    print(run_module(None, input_values, module_filepath, inject_parameters))
