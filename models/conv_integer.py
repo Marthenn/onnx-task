81d2041d1d39a68b837081823b3f51b17b8dc262
Marthenn <89301265+Marthenn@users.noreply.github.com>
2024-12-01 02:39:09 +0700

    refactor (conv_integer): changing input name to accomodate automatic test generation with MatMulInteger script

--- models/conv_integer.py
+++ models/conv_integer.py
@@ -2,22 +2,22 @@ import onnx
 from onnx import helper, TensorProto
 
 def conv_integer(size):
-    input_X = helper.make_tensor_value_info('input_X', TensorProto.UINT8, [1, 1, size, size])
-    input_W = helper.make_tensor_value_info('input_W', TensorProto.UINT8, [1, 1, size, size])
-    input_X_zero_point = helper.make_tensor_value_info('input_X_zero_point', TensorProto.UINT8, [1])
-    input_W_zero_point = helper.make_tensor_value_info('input_W_zero_point', TensorProto.UINT8, [1])
+    input_A = helper.make_tensor_value_info('input_A', TensorProto.UINT8, [1, 1, size, size])
+    input_B = helper.make_tensor_value_info('input_B', TensorProto.UINT8, [1, 1, size, size])
+    # input_A_zero_point = helper.make_tensor_value_info('input_X_zero_point', TensorProto.UINT8, [1])
+    # input_B_zero_point = helper.make_tensor_value_info('input_W_zero_point', TensorProto.UINT8, [1])
     output_Y = helper.make_tensor_value_info('output_Y', TensorProto.INT32, [1, 1, 1, 1])
 
     conv_node = helper.make_node(
         'ConvInteger',
-        inputs=['input_X', 'input_W', 'input_X_zero_point', 'input_W_zero_point'],
+        inputs=['input_A', 'input_B'],
         outputs=['output_Y']
     )
 
     graph = helper.make_graph(
         [conv_node],
         'conv_integer',
-        [input_X, input_W, input_X_zero_point, input_W_zero_point],
+        [input_A, input_B],
         [output_Y]
     )
 
