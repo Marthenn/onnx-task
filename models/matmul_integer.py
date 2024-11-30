343284e972011b2fcee0c62a9dda1d3c12b78ee6
Marthenn <89301265+Marthenn@users.noreply.github.com>
2024-11-30 20:44:57 +0700

    refactor (model): made the model dimension to be free

--- models/matmul_integer.py
+++ models/matmul_integer.py
@@ -2,9 +2,9 @@ import onnx
 from onnx import helper, TensorProto
 
 def matmul_integer():
-    input_A = helper.make_tensor_value_info('input_A', TensorProto.INT8, [1, 1, 2, 2])
-    input_B = helper.make_tensor_value_info('input_B', TensorProto.INT8, [1, 1, 2, 2])
-    output_Y = helper.make_tensor_value_info('output_Y', TensorProto.INT32, [1, 1, 2, 2])
+    input_A = helper.make_tensor_value_info('input_A', TensorProto.INT8, [1, 1, None, None])
+    input_B = helper.make_tensor_value_info('input_B', TensorProto.INT8, [1, 1, None, None])
+    output_Y = helper.make_tensor_value_info('output_Y', TensorProto.INT32, [1, 1, None, None])
 
     matmul_node = helper.make_node(
         'MatMulInteger',
@@ -24,4 +24,4 @@ def matmul_integer():
 
 if __name__ == "__main__":
     model = matmul_integer()
-    onnx.save(model, 'models/matmul_integer.onnx')
+    onnx.save(model, 'matmul_integer.onnx')
