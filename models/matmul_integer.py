cfd19eacbb1979ea8aa9fd9119019e1776db675a
Marthenn <89301265+Marthenn@users.noreply.github.com>
2024-11-30 21:18:32 +0700

    chore (matmul_integer): have predefined shape for matmul_integer

--- models/matmul_integer.py
+++ models/matmul_integer.py
@@ -1,10 +1,10 @@
 import onnx
 from onnx import helper, TensorProto
 
-def matmul_integer():
-    input_A = helper.make_tensor_value_info('input_A', TensorProto.INT8, [1, 1, None, None])
-    input_B = helper.make_tensor_value_info('input_B', TensorProto.INT8, [1, 1, None, None])
-    output_Y = helper.make_tensor_value_info('output_Y', TensorProto.INT32, [1, 1, None, None])
+def matmul_integer(size):
+    input_A = helper.make_tensor_value_info('input_A', TensorProto.INT8, [1, 1, size, size])
+    input_B = helper.make_tensor_value_info('input_B', TensorProto.INT8, [1, 1, size, size])
+    output_Y = helper.make_tensor_value_info('output_Y', TensorProto.INT32, [1, 1, size, size])
 
     matmul_node = helper.make_node(
         'MatMulInteger',
@@ -23,5 +23,7 @@ def matmul_integer():
     return model
 
 if __name__ == "__main__":
-    model = matmul_integer()
-    onnx.save(model, 'matmul_integer.onnx')
+    sizes = [2, 4, 8, 16, 32, 64, 128, 256, 512]
+    for size in sizes:
+        model = matmul_integer(size)
+        onnx.save(model, f'matmul_integer_{size}.onnx')
